657334e9d0bb7ad570d38934d5a296853b771826
Tshegofato <basetsana_12@icloud.com>
2023-08-11 18:21:01 -0700

    My first toolbox!

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+from calculator_1 import add, subtract, multiply, divide
+
+
+a = 10
+b = 5
+
+print(add(a, b))
+print(subtract(a, b))
+print(multiply(a, b))
+print(divide(a, b))
--- 0x02-python-import_modules/README.md
+++ 0x02-python-import_modules/README.md
@@ -2,3 +2,6 @@
 
 task 0.
 "Defines a and b, imports add from add_0 to calculate sum, and prints result."
+
+task 1.
+"imports functions, calculates using given values, and displays results."
