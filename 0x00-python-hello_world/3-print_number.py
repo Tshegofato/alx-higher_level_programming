7cd94bace2b998df719bf787717a2aed2448ef52
Tshegofato <basetsana_12@icloud.com>
2023-08-15 14:14:57 -0700

    Print integer

--- 0x00-python-hello_world/3-print_number.py
+++ 0x00-python-hello_world/3-print_number.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 number = 93
-print(f"{number}, followed by Battery street,\nfollowed by a new line")
+print(f"{number}, followed by Battery street,", end="\nfollowed by a new line")
