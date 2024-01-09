16fa47ecc3fb711dbb41794be7dd25b26ad95b16
Tshegofato <basetsana_12@icloud.com>
2024-01-09 07:38:33 -0800

    Add

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,13 +1,7 @@
 #!/usr/bin/node
-function add(a, b) {
-  return a + b;
+function add (a, b) {
+  const c = a + b;
+  console.log(c);
 }
-const [,, arg1, arg2] = process.argv;
-const num1 = parseInt(arg1);
-const num2 = parseInt(arg2);
-if (isNaN(num1) || isNaN(num2)) {
-  console.log('Invalid arguments. Please provide two integers.');
-} else {
-  const result = add(num1, num2);
-  console.log('The addition of ${num1} and ${num2} is: ${result}');
-}
+
+add(Number(process.argv[2]), Number(process.argv[3]));
