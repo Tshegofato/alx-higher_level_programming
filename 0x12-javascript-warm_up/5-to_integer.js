c414cad3e72033580750b1d11437fa4717792a02
Tshegofato <basetsana_12@icloud.com>
2024-01-09 07:12:26 -0800

    An Integer

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const [,, firstArg] = process.argv;
+const number = parseInt(firstArg);
+if (!isNaN(number)) {
+ console.log(`My number: ${number}`);
+} else {
+  console.log("Not a number");
+}
+
