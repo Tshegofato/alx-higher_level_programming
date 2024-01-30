adafbc72214ffe72439bf98849fd7747aa903656
Tshegofato <basetsana_12@icloud.com>
2024-01-30 10:33:52 -0800

    cURL body size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -2,4 +2,6 @@
 # This script sends a GET request to the provided URL
 # and displays the size of the response body in bytes.
 
-curl - sI "$1" | grep - i "Content-Length" | awk '{print $2}'
+curl - sI "$1" |
+grep - i "content-length" |
+awk '{print $2}'
