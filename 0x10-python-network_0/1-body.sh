82db3ba86352df2a7e48edf369d8739ae2c2dc27
Tshegofato <basetsana_12@icloud.com>
2024-01-30 15:26:34 -0800

    Ex

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # takes in a URL, sends GET request to URL, and displays the body of response
 curl - sfL "$1" - X GET
+
--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # sends DELETE request to URL passed first argument,displays body of response
 curl - s "$1" - X DELETE
+
