5157b5fbf5c76c3ebfedb7487736b3109a4d243a
Tshegofato <basetsana_12@icloud.com>
2024-01-30 15:19:31 -0800

    is

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# sends a DELETE request to the URL passed as the first argument and displays the body of the response
-curl -s "$1" -X DELETE
+# sends DELETE request to URL passed first argument,displays body of response
+curl - s "$1" - X DELETE
