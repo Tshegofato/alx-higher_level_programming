19a4beac74fce941f4467b48aefdc76593230a63
Tshegofato <basetsana_12@icloud.com>
2024-01-30 18:19:07 -0800

    9

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -10,7 +10,7 @@ from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import asc
 
-if _name_ == "_main_":
+if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
                            (sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
@@ -21,4 +21,4 @@ if _name_ == "_main_":
         State.name.like("%a%")).order_by(asc(State.id)).all()
     for i in stmt:
         print("{:d}: {:s}".format(i.id, i.name))
-    session.close()
+    session.close()
