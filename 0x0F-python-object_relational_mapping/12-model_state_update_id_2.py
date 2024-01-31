bc93f4f37a19fba84501214504ece27aa7720fa2
Tshegofato <basetsana_12@icloud.com>
2024-01-30 18:26:04 -0800

    12

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,18 +1,24 @@
 #!/usr/bin/python3
-""" prints the State object with the name passed as argument from the database
+"""
+    script that changes the name of a State
+    object from the database hbtn_0e_6_usa
 """
 import sys
 from model_state import Base, State
+
 from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
-
+from sqlalchemy import asc
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
-    Base.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    new_instance = session.query(State).filter_by(id=2).first()
-    new_instance.name = 'New Mexico'
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
+                           (sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    start = sessionmaker()
+    start.configure(bind=engine)
+    session = start()
+    stmt = session.query(State).filter_by(id=2)
+    for i in stmt:
+        i.name = "New Mexico"
     session.commit()
+    session.close()
