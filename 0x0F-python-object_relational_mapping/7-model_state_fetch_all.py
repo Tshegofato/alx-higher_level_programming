a32c5e3c3b91a917f0597e462e417446c4965ff2
Tshegofato <basetsana_12@icloud.com>
2024-01-30 18:12:42 -0800

    7

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,22 +1,25 @@
 #!/usr/bin/python3
-"""Module that retrieves and prints all\
-        states from a MySQL database using SQLAlchemy."""
+"""
+    script that lists all State objects from the database hbtn_0e_6_usa
+"""
 import sys
-from sqlalchemy import create_engine
+from model_state import Base, State
+
+from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
-from model_state import State
+from sqlalchemy import asc
 
 if __name__ == "__main__":
-    # Create the SQLAlchemy engine using the provided MySQL credentials
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
+                           (sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
-    # Create a session factory
-    Session = sessionmaker(bind=engine)
 
-    # Create a session object
-    session = Session()
+    start = sessionmaker()
+    start.configure(bind=engine)
+    session = start()
+    stmt = session.query(State).order_by(asc(State.id)).all()
 
-    # Retrieve all states from the database and print their IDs and names
-    for state in session.query(State).order_by(State.id):
-        print("{}: {}".format(state.id, state.name))
+    for i in stmt:
+        print("{:d}: {:s}".format(i.id, i.name))
+
+    session.close()
