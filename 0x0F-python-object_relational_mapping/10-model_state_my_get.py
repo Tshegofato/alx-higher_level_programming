96232f862b8bc8c440767fe27e807d60f7fe1b42
Tshegofato <basetsana_12@icloud.com>
2024-01-30 18:21:19 -0800

    10

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,20 +1,27 @@
 #!/usr/bin/python3
-""" prints the State object with the name passed as argument from the database
+"""
+    script that prints the State object with the name passed
+    as argument from the database hbtn_0e_6_usa
 """
 import sys
 from model_state import Base, State
+
 from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
-
+from sqlalchemy import asc
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
-    Base.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    instance = session.query(State).filter(State.name == (sys.argv[4],))
-    try:
-        print(instance[0].id)
-    except IndexError:
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
+                           (sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    arg = sys.argv[4]
+    start = sessionmaker()
+    start.configure(bind=engine)
+    session = start()
+    stmt = session.query(State).filter_by(
+        name=arg).order_by(asc(State.id)).one_or_none()
+    if stmt:
+        print("{:d}".format(stmt.id))
+    else:
         print("Not found")
+    session.close()
